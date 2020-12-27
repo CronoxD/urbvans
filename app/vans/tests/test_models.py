cc3288a38a531c5dbb8b79c6fd50497b3791dc6c
Emmanuel Octavio <emmanueloctaviomc@gmail.com>
2020-12-26 19:57:24 -0600

    Function to create a sample van in test are defined.

--- app/vans/tests/test_models.py
+++ app/vans/tests/test_models.py
@@ -8,21 +8,25 @@ from vans.models import Van
 # Utilities
 import datetime
 
+
+def sample_van(**params):
+    """Create and return a sample van"""
+    defaults = {
+        'plates': 'AW3-123',
+        'economic_number': 'A1-0001',
+        'seats': 6,
+        'status': 'Activa',
+    }
+    defaults.update(params)
+
+    return Van.objects.create(**defaults)
+
+
 class ModelTests(TestCase):
 
     def test_create_a_van_successful(self):
         """Test creating a new van is successful"""
-        plates = 'AW3-123'
-        economic_number = 'A1-0001'
-        seats = 6
-        status = 'Activa'
-
-        van = Van.objects.create(
-            plates=plates,
-            economic_number=economic_number,
-            seats=seats,
-            status=status
-        )
+        van = sample_van()
 
         self.assertEqual(van.plates, plates)
         self.assertEqual(van.economic_number, economic_number)
@@ -32,62 +36,19 @@ class ModelTests(TestCase):
 
     def test_plates_format(self):
         """Test model van don't save when plates format is not correct"""
-        plates = 'AW3-PPP'
-        economic_number = 'A1-0001'
-        seats = 6
-        status = 'Activa'
+        with self.assertRaises(ValueError):
+            sample_van(plates='A1-0001')
 
         with self.assertRaises(ValueError):
-            Van.objects.create(
-                plates=plates,
-                economic_number=economic_number,
-                seats=seats,
-                status=status
-            )
-
-        plates = 'AWS3-123'
-
-        with self.assertRaises(ValueError):
-            Van.objects.create(
-                plates=plates,
-                economic_number=economic_number,
-                seats=seats,
-                status=status
-            )
+            sample_van(plates='AWS3-123')
 
     def test_status_vans(self):
         """Test models van only accept the status 'Activa' and 'En reparación'"""
-        plates = 'AW3-123'
-        economic_number = 'A1-0001'
-        seats = 6
-        status = 'Activa'
-
-        van = Van.objects.create(
-            plates=plates,
-            economic_number=economic_number,
-            seats=seats,
-            status=status
-        )
-
+        van = sample_van(status='Activa')
         self.assertIsNotNone(van.pk)
 
-        status = 'En reparación'
-
-        van = Van.objects.create(
-            plates=plates,
-            economic_number=economic_number,
-            seats=seats,
-            status=status
-        )
-
+        van = sample_van(status='En reparación')
         self.assertIsNotNone(van.pk)
-
-        status = 'this_dont_exists'
 
         with self.assertRaises(ValueError):
-            Van.objects.create(
-                plates=plates,
-                economic_number=economic_number,
-                seats=seats,
-                status=status
-            )
+            sample_van(status='this_status_does_not_exists')
