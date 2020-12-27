fc15a9f7f7cc89067b2fa34af177ace387cf2b86
Emmanuel Octavio <emmanueloctaviomc@gmail.com>
2020-12-27 17:09:37 -0600

    Van list create api view created.

---
+++ app/vans/views/__init__.py
@@ -0,0 +1,1 @@
+from vans.views.van_list_create_api_view import VanListCreateAPIView
---
+++ app/vans/views/van_list_create_api_view.py
@@ -0,0 +1,14 @@
+
+# Django REST Framework
+from rest_framework.generics import ListCreateAPIView
+
+# Models
+from vans.models import Van
+
+# Serializers
+from vans.serializers import VanSerializer
+
+
+class VanListCreateAPIView(ListCreateAPIView):
+    queryset = Van.objects.all()
+    serializer_class = VanSerializer
