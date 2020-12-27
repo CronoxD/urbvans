9b59ad8cdeca9ac228b84a47fe3235b439d4d7dc
Emmanuel Octavio <emmanueloctaviomc@gmail.com>
2020-12-27 00:13:49 -0600

    Van model testing have differents plates

--- app/vans/tests/test_models.py
+++ app/vans/tests/test_models.py
@@ -1,6 +1,7 @@
 
 # Django
 from django.test import TestCase
+from django.db.utils import IntegrityError
 
 # Models
 from vans.models import Van
@@ -35,43 +36,35 @@ class ModelTests(TestCase):
         van = sample_van(**data)
 
         self.assertEqual(van.plates, data['plates'])
-        self.assertEqual(van.economic_number, f'{data["economic_number"]}0001')
+        self.assertEqual(van.economic_number, f'{data["economic_number"]}-0001')
         self.assertEqual(van.seats, data['seats'])
         self.assertEqual(van.status, data['status'])
         self.assertEqual(van.created_at, datetime.date.today())
 
-    def test_plates_format(self):
-        """Test model van don't save when plates format is not correct"""
-        with self.assertRaises(ValueError):
-            sample_van(plates='A1-0001')
-
-        with self.assertRaises(ValueError):
-            sample_van(plates='AWS3-123')
-
     def test_status_vans(self):
         """Test models van only accept the status 'Activa' and 'En reparación'"""
-        van = sample_van(status='Activa')
+        van = sample_van(status='Activa', plates='AW3-444')
         self.assertIsNotNone(van.pk)
 
-        van = sample_van(status='En reparación')
+        van = sample_van(status='En reparación', plates='AW3-111')
         self.assertIsNotNone(van.pk)
 
         with self.assertRaises(ValueError):
-            sample_van(status='this_status_does_not_exists')
+            sample_van(status='this_status_does_not_exists', plates='TAS-122')
 
     def test_plates_are_unique(self):
         """Test plates are unique"""
-        with self.assertRaises(ValueError):
+        with self.assertRaises(IntegrityError):
             sample_van()
             sample_van()
 
     def test_economic_number_secuence(self):
         """Test the economic number secuence generation"""
-        van1 = sample_van(economic_number='NUM')
+        van1 = sample_van(economic_number='NUM', plates='TAX-001')
         self.assertEqual(van1.economic_number, 'NUM-0001')
 
         van2 = sample_van(economic_number='NUM')
-        self.assertEqual(van2.economic_number, 'NUM-0002')
+        self.assertEqual(van2.economic_number, 'NUM-0002', plates='TAX-002')
 
         van3 = sample_van(economic_number='NUM')
-        self.assertEqual(van3.economic_number, 'NUM-0003')
+        self.assertEqual(van3.economic_number, 'NUM-0003', plates='TAX-003')
