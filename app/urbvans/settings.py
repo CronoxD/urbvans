533fef12520bad6334bc9e89eb623b30dcbf7947
Emmanuel Octavio <emmanueloctaviomc@gmail.com>
2020-12-28 16:04:50 -0600

    Cache in request van-list added.

--- app/urbvans/settings.py
+++ app/urbvans/settings.py
@@ -137,3 +137,11 @@ STATIC_URL = '/static/'
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
+
+# Memcached
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': 'cache',
+    }
+}
--- app/vans/views/van_list_create_api_view.py
+++ app/vans/views/van_list_create_api_view.py
@@ -1,4 +1,8 @@
 
+# Django
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
+
 # Django REST Framework
 from rest_framework.generics import ListCreateAPIView
 
@@ -13,3 +17,9 @@ class VanListCreateAPIView(ListCreateAPIView):
     queryset = Van.objects.all()
     serializer_class = VanSerializer
     filterset_fields = ['status']
+
+    # Cache page for the requested url
+    # Cache timeout 1 hour
+    @method_decorator(cache_page(60*60))
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
